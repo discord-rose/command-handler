e744ffbd22bf08ea7d3f7faf6bcf522686cb0aaf
jpbberry <ninja.jpb@gmail.com>
2021-09-11 18:40:13 -0400

    fix: git issue with file names

---
+++ src/Symbols.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+export class Symbols {
+  static prefix = '__jadl.'
+
+  static readonly commandName = Symbol(Symbols.prefix + 'command-name')
+  static readonly aliases = Symbol(Symbols.prefix + 'aliases')
+  static readonly commands = Symbol(Symbols.prefix + 'commands')
+  static readonly interaction = Symbol(Symbols.prefix + 'interaction')
+
+  static readonly baseCommand = Symbol(Symbols.prefix + 'base-command')
+}
