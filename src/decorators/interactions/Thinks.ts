9d527046e6d2ff8e1188b4d284657e47c07f7ea1
Cameron Maclean <34384474+BowsiePup@users.noreply.github.com>
2022-06-29 21:04:25 +0000

    feat: Ephemeral response on thinks

--- src/decorators/interactions/Thinks.ts
+++ src/decorators/interactions/Thinks.ts
@@ -1,13 +1,22 @@
-import { InteractionResponseType } from 'discord-api-types/v9'
+import { APIInteractionResponse, InteractionResponseType, MessageFlags } from 'discord-api-types/v9'
 import { Decorators } from '../../utils/Decorators'
+import { EphemeralEmbed } from '../../utils/EphemeralEmbed'
 
-export const Thinks = Decorators.createCommandDecorator((_opt, cmd) => {
+export const Thinks = Decorators.createCommandDecorator<[ephemeral?: boolean]>(
+  ([ephemeral], cmd) => {
   cmd.onRun.push(async (int, { worker }) => {
     int.responded = true
+
+    const body: APIInteractionResponse = {
+      type: InteractionResponseType.DeferredChannelMessageWithSource
+    }
+
+    if (ephemeral === true) body.data = {
+      flags: MessageFlags.Ephemeral
+    }
+
     await worker.api.post(`/interactions/${int.id}/${int.token}/callback`, {
-      body: {
-        type: InteractionResponseType.DeferredChannelMessageWithSource
-      }
+      body
     })
   })
 })
